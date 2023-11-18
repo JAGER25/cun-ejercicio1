b7dca5da423af3a23d46615b806baa8fba34c4a0
JAGER25 <jagerrubio@gmail.com>
2023-11-18 13:53:18 -0500

    Entrega Final

--- Ejercicio1/Program.cs
+++ Ejercicio1/Program.cs
@@ -7,71 +7,61 @@ Console.ReadKey();
 Console.WriteLine("##########################  Impares o Pares  ##########################");
 
 //Variables 
-int numeroJuegos = 0;
-List<TarjetasDto> rojas = new();
-List<TarjetasDto> azules = new();
+List<List<TarjetasDto>> rojas = new();
+List<List<TarjetasDto>> azules = new();
 
-
-//Constantes
-const string Jugador1 = "Maria";
-const string Jugador2 = "Juan";
+List<TarjetasDto> lista1 = new();
+List<TarjetasDto> lista2 = new();
+int numeroJuegos = 1;
 
 const int maxRepeticiones = 100;
 const int maxNumXJuego = 5;
-
 
 //Logica para iniciar el juego
 Console.WriteLine();
 Console.WriteLine();
 Console.WriteLine();
-Console.WriteLine("Ingrese el número de juegos: ");
 
-
-do
+while (numeroJuegos > 0)
 {
-    numeroJuegos = Convert.ToInt32(Console.ReadLine());
-    if (numeroJuegos <= 0 || numeroJuegos > maxRepeticiones)
+    Console.WriteLine();
+    Console.WriteLine("****************************************************************************");
+    Console.WriteLine("Ingrese el número de repeticiones: ");
+
+    do
     {
-        Console.WriteLine("!!!! La cantidad de repeticiones no puede ser menor a 0 ni mayor a 100 !!!!");
-        Console.WriteLine("Intenta nuevamente");
+        numeroJuegos = Convert.ToInt32(Console.ReadLine());
+        if (numeroJuegos < 0 || numeroJuegos > maxRepeticiones)
+        {
+            Console.WriteLine("!!!! La cantidad de repeticiones no puede ser menor a 0 ni mayor a 100 !!!!");
+            Console.WriteLine("Intenta nuevamente");
+        }
+
+    } while (numeroJuegos < 0 || numeroJuegos > maxRepeticiones);
+
+    if (numeroJuegos > 0)
+    {
+        var result = Tools.Operaciones(numeroJuegos, maxNumXJuego);
+        azules.Add(result.Item1);
+        rojas.Add(result.Item2);
     }
-    
-} while (numeroJuegos <= 0 || numeroJuegos > maxRepeticiones);
 
-Console.WriteLine();
-Console.WriteLine($"Se jugaran {numeroJuegos} repeticiones.");
-Console.WriteLine();
-Console.WriteLine("Presione cualquier tecla para iniciar juego, buena suerte.");
-Console.WriteLine();
-Console.WriteLine("Ingrese el numero ");
-
-//Se iteran la cantidad de repeticiones 
-for (int i = 1; i <= numeroJuegos; i++)
-{
-    Console.WriteLine($"Ingresar el número de {Jugador1}");
-    azules.Add(Tools.Insertar(Jugador1,i,maxNumXJuego));
 }
 
-for (int i = 1; i <= numeroJuegos; i++)
-{
-    Console.WriteLine($"Ingresar el número de {Jugador2}");
-    rojas.Add(Tools.Insertar(Jugador2, i, maxNumXJuego));
-}
 
 Console.WriteLine("!!!!!!!!!! Juego finalizado !!!!!!!!!!");
-Console.WriteLine("Presione cualquier tecla para obtener los resultados");
-Console.ReadKey();
+//Console.WriteLine("Presione cualquier tecla para obtener los resultados");
+//Console.ReadKey();
 
-var result = Tools.ObternerResultados(azules, rojas);
 
 Console.WriteLine("######################## Resultado ########################");
 Console.WriteLine();
-Console.WriteLine($"{Jugador1} puedo haber ganado: {result.Item1}");
-Console.WriteLine($"{Jugador2} puedo haber ganado: {result.Item2}");
 
-
-
-
-
+for (int i = 0; i < azules.Count; i++)
+{
+    var result = Tools.ObternerResultados(azules[i], rojas[i]);
+    if (result.Item2 > result.Item1) Console.WriteLine(result.Item2 - result.Item1);
+    else Console.WriteLine(result.Item2 - result.Item1);
+}
 
 
--- Ejercicio1/Tools.cs
+++ Ejercicio1/Tools.cs
@@ -2,11 +2,39 @@
 {
     public static class Tools
     {
+        public static (List<TarjetasDto>, List<TarjetasDto>) Operaciones(int numeroJuegos, int maxNumXJuego)
+        {
+            List<TarjetasDto> lista1 = new();
+            List<TarjetasDto> lista2 = new();
+
+            //Constantes
+            const string Jugador1 = "Maria";
+            const string Jugador2 = "Juan";
+
+            Console.WriteLine();
+
+            //Se iteran la cantidad de repeticiones 
+            for (int i = 1; i <= numeroJuegos; i++)
+            {
+                Console.WriteLine($"Ingresar el número de {Jugador1}");
+
+                lista1.Add(Tools.Insertar(Jugador1, i, maxNumXJuego));
+            }
+
+            for (int i = 1; i <= numeroJuegos; i++)
+            {
+                Console.WriteLine($"Ingresar el número de {Jugador2}");
+                lista2.Add(Tools.Insertar(Jugador2, i, maxNumXJuego));
+            }
+
+            return (lista1, lista2);
+
+        }
         public static TarjetasDto Insertar(string jugador, int i, int maxNumXJuego)
         {
             int numeroSeleccionado = 0;
             Console.WriteLine($"Turno {i}.");
-            Console.WriteLine();            
+            Console.WriteLine();
             do
             {
                 numeroSeleccionado = Convert.ToInt32(Console.ReadLine());
@@ -21,25 +49,21 @@
             return new TarjetasDto() { Position = i, Numero = numeroSeleccionado };
         }
 
-        public static (int, int) ObternerResultados(List<TarjetasDto> azules, List<TarjetasDto> rojas) 
+        public static (int, int) ObternerResultados(List<TarjetasDto> azules, List<TarjetasDto> rojas)
         {
             int resulAzules = 0, resulRojas = 0;
 
             foreach (var i in azules)
             {
-                foreach (var j in rojas)
-                {
-                    if(ValidPar(i.Numero + j.Numero))resulRojas++;
-                    else resulAzules++;
-                }
+                if ((i.Numero & 1) == 1) resulAzules++;
+            }
+
+            foreach (var j in rojas)
+            {
+                if ((j.Numero & 1) == 0) resulRojas++;
             }
             return (resulAzules, resulRojas);
         }
 
-        private static bool ValidPar(int numero)
-        {
-            int residuo = numero % 2;
-            return residuo == 0;
-        }
     }
 }
